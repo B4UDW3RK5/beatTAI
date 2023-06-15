258385a14fddd91fb47beaba6b9c885d3bf6d7ae
cat K <cat@ff3366.com>
2023-06-15 23:15:42 +0930

    moved i9w to it's own directory and added a beatTAI clock written in Go

--- gbt/gbt.go
+++ gbt/gbt.go
@@ -6,21 +6,25 @@ import (
 )
 
 func main() {
-   // Variable to store the number of leap seconds
+   // variable to store the number of leap seconds. this is really lazy but
+   // later I'll find a way to snag the current number of leap seconds, until
+   // then this will need to be manually changed any time a new leap second is
+   // added.
    leapSeconds := 37
 
-   // Get the current time in UTC
+   // grab the current time in UTC
    currentTime := time.Now().UTC()
 
-   // Extract the hour, minute, and second from the current time
-   hour := currentTime.Hour()
-   minute := currentTime.Minute()
-   second := currentTime.Second()
+   // pull the hour, minute, and second from the current time and convert the
+   // hour and minute to seconds
+   hourSeconds := currentTime.Hour() * 3600
+   minuteSeconds := currentTime.Minute() * 60
+   seconds := currentTime.Second()
 
-   // Print the values
-   fmt.Println("Current UTC Time:")
-   fmt.Println("Hour:", hour)
-   fmt.Println("Minute:", minute)
-   fmt.Println("Second:", second)
-   fmt.Println("leapSeconds:", leapSeconds)
+   // do the math for beatTAI and store it
+   beatTAI := float64((hourSeconds + minuteSeconds + seconds + leapSeconds)) / 86.4
+
+   // print the value, rounded to two decimal places, padded with leading zeroes
+   // if necessary and prefixed with : for proper beatTAI syntax
+   fmt.Printf(":%06.2f\n", beatTAI)
 }
