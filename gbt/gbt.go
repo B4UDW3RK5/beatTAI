e6fdcc6cbf5c71a42fde94d3833a45b99739fb00
D. Bohdan <dbohdan@dbohdan.com>
2024-11-28 21:29:57 +0000

    gbt: format with `go fmt`

--- gbt/gbt.go
+++ gbt/gbt.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-   "fmt"
-   "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-   // variable to store the number of leap seconds. this is really lazy but
-   // later I'll find a way to snag the current number of leap seconds, until
-   // then this will need to be manually changed any time a new leap second is
-   // added.
-   leapSeconds := 37
+	// variable to store the number of leap seconds. this is really lazy but
+	// later I'll find a way to snag the current number of leap seconds, until
+	// then this will need to be manually changed any time a new leap second is
+	// added.
+	leapSeconds := 37
 
-   // grab the current time in UTC
-   currentTime := time.Now().UTC()
+	// grab the current time in UTC
+	currentTime := time.Now().UTC()
 
-   // pull the hour, minute, and second from the current time and convert the
-   // hour and minute to seconds
-   hourSeconds := currentTime.Hour() * 3600
-   minuteSeconds := currentTime.Minute() * 60
-   seconds := currentTime.Second()
+	// pull the hour, minute, and second from the current time and convert the
+	// hour and minute to seconds
+	hourSeconds := currentTime.Hour() * 3600
+	minuteSeconds := currentTime.Minute() * 60
+	seconds := currentTime.Second()
 
-   // do the math for beatTAI and store it
-   beatTAI := float64((hourSeconds + minuteSeconds + seconds + leapSeconds)) / 86.4
+	// do the math for beatTAI and store it
+	beatTAI := float64((hourSeconds + minuteSeconds + seconds + leapSeconds)) / 86.4
 
-   // print the value, rounded to two decimal places, padded with leading zeroes
-   // if necessary and prefixed with : for proper beatTAI syntax
-   fmt.Printf(":%06.2f\n", beatTAI)
+	// print the value, rounded to two decimal places, padded with leading zeroes
+	// if necessary and prefixed with : for proper beatTAI syntax
+	fmt.Printf(":%06.2f\n", beatTAI)
 }
