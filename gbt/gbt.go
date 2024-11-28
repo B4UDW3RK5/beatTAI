9a03b09ac7a8087e245fff89c8423c25a0e49467
D. Bohdan <dbohdan@dbohdan.com>
2024-11-28 21:32:35 +0000

    gbt: use tai library to not hardcode leap seconds

--- gbt/gbt.go
+++ gbt/gbt.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/brandondube/tai"
 )
 
 func main() {
-	// variable to store the number of leap seconds. this is really lazy but
-	// later I'll find a way to snag the current number of leap seconds, until
-	// then this will need to be manually changed any time a new leap second is
-	// added.
-	leapSeconds := 37
+	g := tai.Now().AsGregorian()
 
-	// grab the current time in UTC
-	currentTime := time.Now().UTC()
-
-	// pull the hour, minute, and second from the current time and convert the
+	// pull the hour, minute, and second from the TAI time and convert the
 	// hour and minute to seconds
-	hourSeconds := currentTime.Hour() * 3600
-	minuteSeconds := currentTime.Minute() * 60
-	seconds := currentTime.Second()
+	hourSeconds := g.Hour * 3600
+	minuteSeconds := g.Min * 60
+	seconds := g.Sec
 
 	// do the math for beatTAI and store it
-	beatTAI := float64((hourSeconds + minuteSeconds + seconds + leapSeconds)) / 86.4
+	beatTAI := float64(hourSeconds+minuteSeconds+seconds) / 86.4
 
 	// print the value, rounded to two decimal places, padded with leading zeroes
 	// if necessary and prefixed with : for proper beatTAI syntax
